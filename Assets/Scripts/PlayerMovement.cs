87f209600d56a7a66e78bed0229b794d0fa7c31e
MADHUR KUMAR <madhurchugh360@gmail.com>
2024-08-15 13:46:36 +0530

    Added Player Sprites and Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 movedirection;
+
+    Rigidbody2D playerBody;
+    void Awake()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Processing Input
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        //Physics Calculations
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        movedirection = new Vector2(moveX, moveY);
+        movedirection.Normalize();
+    }
+
+    void Move()
+    {
+        playerBody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
     }
 }
