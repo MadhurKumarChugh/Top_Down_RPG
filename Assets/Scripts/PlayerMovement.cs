e5fdd27e606bc81192a1ed33f5c5a93602ddf575
MADHUR KUMAR <madhurchugh360@gmail.com>
2024-08-16 12:22:24 +0530

    Added Collision to Tilemap and Player Also edited spritesheet custom physics shape, so that tilemap collider can be customized

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Movement Speed of Player
     [SerializeField] float moveSpeed;
+    
+    // A movement vector to store input from user
     [SerializeField] Vector2 movedirection;
-
+    
+    // Rigidbody object component of player
     Rigidbody2D playerBody;
+    
     void Awake()
     {
+        // Getting the Rigidbody2D component of the player
         playerBody = GetComponent<Rigidbody2D>();
     }
 
@@ -28,15 +34,28 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInput()
     {
+        // Getting input from user
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-
+        
+        // *****OPTIONAL*****
+        // Cancel diagonal movement
+        // if (moveX != 0)
+        // {
+        //     moveY = 0;
+        // }
+        
+        // Storing the input in movedirection vector
         movedirection = new Vector2(moveX, moveY);
+        
+        // To normalize diagonal movement
+        // If not implemented, diagonal movement will be higher than left-right,up-down movement
         movedirection.Normalize();
     }
 
     void Move()
     {
+        // Using velocity to move player
         playerBody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
     }
 }
