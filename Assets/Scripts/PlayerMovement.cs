d3da39af164115ab3fd1aa99be05ee4ece314f72
MADHUR KUMAR <madhurchugh360@gmail.com>
2024-08-17 18:25:09 +0530

    Bugs related to Show Message Coroutine were fixed Show Message still needs some polish

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     // Movement Speed of Player
     [SerializeField] float moveSpeed;
-    bool interacted = false;
-    bool isTouched = false;
+    // A boolean to check we have contacted the NPC or not
+    bool isTouched;
+    // Waiting time before ShowMessage coroutine starts
+    [SerializeField] float waitTime = 2f;
     
     // A movement vector to store input from user
     [SerializeField] Vector2 movedirection;
@@ -26,7 +28,13 @@ public class PlayerMovement : MonoBehaviour
     {
         //Processing Input
         ProcessInput();
-        ShowMessage();
+        
+        // Checking if key is pressed after contacting NPC
+        if (Input.GetKey(KeyCode.Space) && isTouched)
+        {
+            // Started Show Message coroutine
+            StartCoroutine(Interact());
+        }
     }
 
     void FixedUpdate()
@@ -37,62 +45,34 @@ public class PlayerMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        // Checking if player has contacted NPC or not
         if (other.gameObject.CompareTag("NPC"))
         {
+            // If player has contacted NPC, isTouched is set to true
             isTouched = true;
         }
     }
     
-    // void OnCollisionStay2D(Collision2D other)
-    // {
-    //     if (other.gameObject.CompareTag("NPC") && interacted)
-    //     {
-    //         Debug.Log("NPC is saying something");
-    //     }
-    // }
-    
-    // void OnCollisionExit2D(Collision2D other)
-    // {
-    //     if (other.gameObject.CompareTag("NPC") && interacted)
-    //     {
-    //         Debug.Log("NPC is saying something");
-    //     }
-    // }
-
     IEnumerator Interact()
     {
         int count = 0;
         // Debug.Log("Startrd");
-        while(interacted && isTouched)
+        // Started loop of coroutine if contacted NPC
+        // Can be changed to a for loop later on
+        while(isTouched)
         {
-            yield return new WaitForSeconds(0);
+            // Waiting before next message is shown
+            yield return new WaitForSeconds(waitTime);
             count++;
             Debug.Log(count);
         }
     }
-
-    void ShowMessage()
-    {
-        StartCoroutine(Interact());
-    }
     
-
-    
-
     void ProcessInput()
     {
         // Getting input from user
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            interacted = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            interacted = false;
-        }
         
         // *****OPTIONAL*****
         // Cancel diagonal movement
@@ -100,9 +80,7 @@ public class PlayerMovement : MonoBehaviour
         {
             moveY = 0;
         }
-
         
-        // Interact(moveX, moveY);
         
         // Storing the input in movedirection vector
         movedirection = new Vector2(moveX, moveY);
