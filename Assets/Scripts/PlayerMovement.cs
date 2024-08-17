e609ea99b92d9b06f3701a6b846ab4a164c620cc
MADHUR KUMAR <madhurchugh360@gmail.com>
2024-08-17 15:49:22 +0530

    Added a NPC and basic collision Bugs related to Show Message need to be fixed Bugs related to Coroutine need to be fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Movement Speed of Player
     [SerializeField] float moveSpeed;
+    bool interacted = false;
+    bool isTouched = false;
     
     // A movement vector to store input from user
     [SerializeField] Vector2 movedirection;
@@ -24,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //Processing Input
         ProcessInput();
+        ShowMessage();
     }
 
     void FixedUpdate()
@@ -32,18 +35,74 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            isTouched = true;
+        }
+    }
+    
+    // void OnCollisionStay2D(Collision2D other)
+    // {
+    //     if (other.gameObject.CompareTag("NPC") && interacted)
+    //     {
+    //         Debug.Log("NPC is saying something");
+    //     }
+    // }
+    
+    // void OnCollisionExit2D(Collision2D other)
+    // {
+    //     if (other.gameObject.CompareTag("NPC") && interacted)
+    //     {
+    //         Debug.Log("NPC is saying something");
+    //     }
+    // }
+
+    IEnumerator Interact()
+    {
+        int count = 0;
+        // Debug.Log("Startrd");
+        while(interacted && isTouched)
+        {
+            yield return new WaitForSeconds(0);
+            count++;
+            Debug.Log(count);
+        }
+    }
+
+    void ShowMessage()
+    {
+        StartCoroutine(Interact());
+    }
+    
+
+    
+
     void ProcessInput()
     {
         // Getting input from user
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            interacted = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            interacted = false;
+        }
+        
         // *****OPTIONAL*****
         // Cancel diagonal movement
-        // if (moveX != 0)
-        // {
-        //     moveY = 0;
-        // }
+        if (moveX != 0)
+        {
+            moveY = 0;
+        }
+
+        
+        // Interact(moveX, moveY);
         
         // Storing the input in movedirection vector
         movedirection = new Vector2(moveX, moveY);
@@ -58,4 +117,11 @@ public class PlayerMovement : MonoBehaviour
         // Using velocity to move player
         playerBody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
     }
+
+    // void Interact(float movX = 0, float movY = 0)
+    // {
+    //     var facingDirection = new Vector3(movX,movY);
+    //     var interactPos = transform.position + facingDirection;
+    //     Debug.DrawLine(transform.position, interactPos, Color.red,0.5f);
+    // }
 }
