676ddc9fa64b9848bc1e6c7fc64e88400f57477d
MadhurKumarChugh <madhurchugh360@gmail.com>
2024-08-27 19:48:16 +0530

    Completed Player Walk Down and Attack Animation Other 3 Animations Incomplete Also need to constraint player movement while attacking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // walkDown Parameter of Animator
+    private static readonly int WalkDown = Animator.StringToHash("walkDown");
+    // isAttacking Parameter of Animator
+    private static readonly int IsAttacking = Animator.StringToHash("isAttacking");
+
     [Header("PLAYER MOVEMENT")]
     // Movement Speed of Player
     [SerializeField] float moveSpeed;
@@ -12,11 +17,15 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movedirection;
     // Rigidbody object component of player
     Rigidbody2D playerBody;
+    // Animator to control player animation by code
+    Animator playerAnimator;
     
     void Awake()
     {
         // Getting the Rigidbody2D component of the player
         playerBody = GetComponent<Rigidbody2D>();
+        // Getting the Animator component of the player
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,14 +48,14 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         
-        Debug.Log(moveX);
-        
         // *****OPTIONAL*****
         // Cancel diagonal movement
         if (moveX != 0)
         {
             moveY = 0;
         }
+        
+        AnimatePlayer(moveX, moveY);
         
         
         // Storing the input in movedirection vector
@@ -61,6 +70,39 @@ public class PlayerMovement : MonoBehaviour
     {
         // Using velocity to move player
         playerBody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
+    }
+    
+    // Function to control all animations
+    void AnimatePlayer(float x, float y)
+    {
+        DownAnim(y);
+        AttackChecker();
+    }
+
+    // Function to check if player is attacking or not
+    void AttackChecker()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnimator.SetBool(IsAttacking,true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerAnimator.SetBool(IsAttacking,false);
+        }
+    }
+
+    // Function for WalkDown animation
+    void DownAnim(float y)
+    {
+        if (y.Equals(-1))
+        {
+            playerAnimator.SetBool(WalkDown, true);
+        }
+        else if (y.Equals(0))
+        {
+            playerAnimator.SetBool(WalkDown, false);
+        }
     }
 
     // void Interact(float movX = 0, float movY = 0)
